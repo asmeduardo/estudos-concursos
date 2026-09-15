fa3f9549ef689fb384d87c21ed34fc2d1622ac3a
Eduardo Alessandro Santana de Melo <eduardo.melo01992@gmail.com>
2026-09-14 23:32:22 -0300

    fix: disable service worker caching

--- sw.js
+++ sw.js
@@ -1,16 +1,2 @@
-const CACHE = 'nexame-shell-v2';
-const SHELL = [
-  './', './index.html', './dashboard.js', './auth.html', './cadastro.html', './recuperar-senha.html', './redefinir-senha.html', './auth.js', './auth.css', './favicon.svg', './site.webmanifest',
-  './assets/brand/nexame-mark.svg', './assets/icons/nexame-192.png', './assets/icons/nexame-512.png'
-];
-
-self.addEventListener('install', (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)).then(() => self.skipWaiting())));
-self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET' || new URL(event.request.url).origin !== self.location.origin) return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => {
-    const copy = response.clone();
-    caches.open(CACHE).then((cache) => cache.put(event.request, copy));
-    return response;
-  })));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key)))).then(() => self.registration.unregister())));
